02dac08b9a931b0ce43a41a35503b0c664802966
WeirdestYeti <juristheyeti@gmail.com>
2017-09-17 14:53:40 +0200

    added migration

---
+++ Web_Api/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public ItemCategory Category { get; set; }
+        public Account OwnerAccount { get; set; }
+        public double PointValue { get; set; }
+    }
+}
