c66290926198240636ecd59a04046185540f8e21
WeirdestYeti <juristheyeti@gmail.com>
2017-09-20 11:46:23 +0200

    returning only needed values

---
+++ Web_Api/Models/Response/Item/ItemResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models.Response.Item
+{
+    public class ItemResponse : IResponse
+    {
+        public IEnumerable<string> Errors { get; set; }
+
+        public int Id { get; set; }
+        public ItemCategory Category { get; set; }
+        public string OwnerAccountId { get; set; }
+        public double PointValue { get; set; }
+    }
+}
